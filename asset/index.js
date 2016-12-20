af68a7aecd48b9f7a30a4300f1da0be866f124b8
wurui <wurui.wr@gmail.com>
2016-12-20 18:41:10 +0800

    first commit

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,94 @@
-define([],function(){
-  return {
-    init:function($mod){
+define([], function () {
 
+    var scrollto=function(box,left,fn){
+
+        if(box.scrollLeft!=left) {
+
+            var delta=(left-box.scrollLeft)/10;
+            //console.log('origin',delta)
+            delta=Math.abs(delta)<1?(delta>0?1:-1):delta;
+//console.log(delta,box.scrollLeft,left)
+            box.scrollLeft+=delta;
+            setTimeout(function () {
+                scrollto(box, left,fn);
+            },16)
+        }else{
+            fn && fn()
+        }
+    };
+    var checkScroll=function(box,fn){
+        var width=box.clientWidth,
+            scrollLeft=box.scrollLeft,
+            ratio=scrollLeft/width,
+            index=Math.round(ratio);
+        //var delta=scrollLeft-index*width;
+        //console.log(delta,index)
+        //box.style.transform='translate('+delta+'px,0)'
+        scrollto(box,index*width,function(){
+            fn({index:index})
+        })
+        //box.scrollLeft=index * width;
+    };
+    return {
+        init: function ($mod) {
+            var TO_Scroll;
+
+
+            var touchOnDesc=false;
+            var $box=$mod.find('.J_box').on('tap', function (e) {
+                //console.log('sd')
+                $mod.toggleClass('fullscreen')
+            }).on('swipe', function (e) {
+                //var type= e.type;
+                //console.log('swipe', e);
+
+
+            }).on('scroll',function(){
+                if(TO_Scroll){
+                    clearTimeout(TO_Scroll)
+                }
+                TO_Scroll=setTimeout(function(){
+                    checkScroll($box[0],function(obj){
+                        var desc=$box.children().eq(obj.index).attr('data-text');
+                        $desc.css({
+                            transform:'translate(0,0)'
+                        }).data('delta',0).find('.J_desc_text').html(desc);
+                        $desc.find('.J_desc_index').html(obj.index+1);
+                        $mod.attr('data-index',obj.index+1)
+                    });
+                },100);
+
+
+
+            }),
+            $desc=$mod.find('.J_desc').on('touchstart',function(e){
+                touchOnDesc={
+                    startX: e.touches[0].clientX,
+                    startY: e.touches[0].clientY,
+                    lastDelta:($desc.data('delta')||0)-0
+                };
+                return false;
+
+            });
+            $mod.on('touchmove',function(e){
+                if(touchOnDesc){
+                    var delta=Math.max(minDescY,Math.min(0,e.touches[0].clientY-touchOnDesc.startY+touchOnDesc.lastDelta));
+
+                    $desc.css({
+                        transform:'translate(0,'+delta+'px)'
+                    }).data('delta',delta)
+                }
+            }).on('touchend touchcancel',function(e){
+                if(touchOnDesc){
+                    touchOnDesc=null;
+                }
+            });
+
+            var minDescY=$mod.offset().top-$desc.offset().top;
+            //console.log(maxDescY)
+
+
+
+        }
     }
-  }
 })
--- mod.xsl
+++ mod.xsl
@@ -1,13 +1,22 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
     <xsl:template match="/root" name="wurui.imgplayer">
-    <!-- className 'J_OXMod' required  -->
-    <div class="J_OXMod oxmod-imgplayer" ox-mod="imgplayer">
-
-      <h1>
-        This is mod imgplayer;
-      </h1>
-      </div>
+        <!-- className 'J_OXMod' required  -->
+        <xsl:variable name="imgs" select="data/imgplayer/i"/>
+        <div class="J_OXMod oxmod-imgplayer" data-src="asset/test.png" data-index="1" ox-mod="imgplayer">
+            <div class="img-box J_box">
+                <xsl:for-each select="$imgs">
+                    <span data-text="{text}" style="background-image:url({src})" class="img-item">
+                    </span>
+                </xsl:for-each>
+            </div>
+            <div class="img-desc J_desc">
+                <label class="desc-index"><em class="J_desc_index">1</em>/<xsl:value-of select="count($imgs)"/></label>
+                <div class="J_desc_text desc-text">
+                    <xsl:value-of select="$imgs[position()=1]/text"/>
+                </div>
+            </div>
+        </div>
     </xsl:template>
 
 </xsl:stylesheet>
